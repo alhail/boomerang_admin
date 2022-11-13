12c6ca8cdff66305f0840e0cbcd6ec1e7b41aa5b
alhail <mohammad2001m@yahoo.com>
2022-11-13 22:55:44 +0100

    add a sizebox to sendt the code and its status

---
+++ android/app/src/assignCodePage.dart
@@ -0,0 +1,13 @@
+class AssignCodePage extends StatefulWidget {
+  const AssignCodePage({Key? key}, String code) : super(key: key);
+
+  @override
+  State<AssignCodePage> createState() => _AssignCodePageState(code);
+}
+
+class _AssignCodePageState extends State<AssignCodePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
